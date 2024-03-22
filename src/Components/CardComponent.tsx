56b38f69ac97ce38a7e83a9dd95e48877639df3a
Aiden Foo <aidenfoo154@gmail.com>
2024-03-22 13:56:40 -0700

    Post Peer Review, Card BorderTop Changes

--- src/Components/CardComponent.tsx
+++ src/Components/CardComponent.tsx
@@ -11,7 +11,7 @@ export default function CardComponent() {
     <>
     
     <section className="py-10 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
-      <article className="bg-slate-200 dark:bg-slate-800 p-5 rounded-sm  dark:shadow-slate-700 border-t-8 border-blue-600 hover:bg-slate-300 dark:hover:bg-slate-700">
+      <article className="bg-slate-200 dark:bg-slate-800 p-5 rounded-md dark:shadow-slate-700 border-t-4 border-blue-600 hover:bg-slate-300 dark:hover:bg-slate-700">
         <ul className="flex items-center justify-center text-center mt-3">
           <li>
             <img src={facebook} alt="" className="mr-2"/>
@@ -26,7 +26,7 @@ export default function CardComponent() {
         </p>
       </article>
 
-      <article className="bg-slate-200 dark:bg-slate-800 p-5 rounded-sm  dark:shadow-slate-700 border-t-8 border-blue-500 hover:bg-slate-300 dark:hover:bg-slate-700">
+      <article className="bg-slate-200 dark:bg-slate-800 p-5 rounded-md dark:shadow-slate-700 border-t-4 border-blue-500 hover:bg-slate-300 dark:hover:bg-slate-700">
         <ul className="flex items-center justify-center text-center mt-3">
           <li>
             <img src={twitter} alt="" className="mr-2"/>
@@ -41,8 +41,8 @@ export default function CardComponent() {
         </p>
       </article>
 
-      <article className="bg-slate-200 dark:bg-slate-800 p-5 rounded-sm  dark:shadow-slate-700 relative hover:bg-slate-300 dark:hover:bg-slate-700">
-        <div className="bg-gradient-to-r from-[#fdc468] to-[#df4996] h-2 w-full rounded-tl-sm rounded-tr-sm absolute top-0 left-0"></div>
+      <article className="bg-slate-200 dark:bg-slate-800 p-5 rounded-md dark:shadow-slate-700 relative hover:bg-slate-300 dark:hover:bg-slate-700">
+        <div className="bg-gradient-to-r from-[#fdc468] to-[#df4996] h-1 w-full rounded-tl-sm rounded-tr-sm absolute top-0 left-0"></div>
         <ul className="flex items-center justify-center text-center mt-3">
           <li>
             <img src={instagram} alt="" className="mr-2"/>
@@ -57,7 +57,7 @@ export default function CardComponent() {
         </p>
       </article>
 
-      <article className="bg-slate-200 dark:bg-slate-800 p-5 rounded-sm  dark:shadow-slate-700 border-t-8 border-red-700 hover:bg-slate-300 dark:hover:bg-slate-700 border-solid">
+      <article className="bg-slate-200 dark:bg-slate-800 p-5 rounded-md dark:shadow-slate-700 border-t-4 border-red-700 hover:bg-slate-300 dark:hover:bg-slate-700 border-solid">
         <ul className="flex items-center justify-center text-center mt-3">
           <li>
             <img src={youtube} alt="" className="mr-2"/>
